459b286e673cc0b1d28624ebeea39eb7625930b7
Ahmad Zulfikar <ahmadzulfikarproject@gmail.com>
2022-02-10 18:12:06 +0700

    test

--- resources/views/backend/students.blade.php
+++ resources/views/backend/students.blade.php
@@ -1,7 +1,7 @@
 @extends('backend.master')
 
 @section('content')
-<div class="container" id="app">
+<div class="containerz" id="app">
     <div class="row mt-5">
         <div class="col-md-12">
             <div class="card">
